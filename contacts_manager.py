b95b75c286d6b625bbbfa2adfe4a9c54d36284cd
Aneta <KakasovaAneta@seznam.cz>
2025-06-05 20:45:58 +0200

    Přidání hlavního menu v2

--- contacts_manager.py
+++ contacts_manager.py
@@ -15,3 +15,4 @@ def hlavni_menu():
     print("1. Zobrazit kontakty\n2. Přidat kontakt\n3. Ukončit")
     # načte vstup od uživatele
     vyber_funkce = input("Vyberte funkci 1-3: ")
+    print("Byla vybrána funkce: " + vyber_funkce)
