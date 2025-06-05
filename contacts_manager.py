d55cd1b38d3bf9e1f9880cb9e0e35d4ebb8297d4
Aneta <KakasovaAneta@seznam.cz>
2025-06-05 20:43:41 +0200

    Přidání hlavního menu

---
+++ contacts_manager.py
@@ -0,0 +1,17 @@
+contacts = [
+    {
+        "name": "Anička",
+        "email": "anicka@email.com",
+        "phone": "777 777 777"
+    },
+    {
+        "name": "Nikol",
+        "email": "nikol@email.com",
+        "phone": "777 777 777"
+    }
+]
+
+def hlavni_menu():
+    print("1. Zobrazit kontakty\n2. Přidat kontakt\n3. Ukončit")
+    # načte vstup od uživatele
+    vyber_funkce = input("Vyberte funkci 1-3: ")
